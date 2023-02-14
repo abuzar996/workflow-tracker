9cc10643e5cf1bab1648937dfe80999ee79ada30
abuzar <abuzar.rahim70@gmail.com>
2023-02-15 02:38:25 +0500

    first commit

--- components/assets/Button/button.js
+++ components/assets/Button/button.js
@@ -3,33 +3,7 @@ import styles from './button.module.css';
 import './button.module.css';
 const Button = ({children,size,color}) => {
     const [className, setClassName] =useState(null);
-    useEffect(()=>{
-        if(size==='md')
-        {
-            setClassName(styles.buttonContainerMd)
-        }
-        else if(size==='sm')
-        {
-            setClassName(styles.buttonContainerSm)
-        }
-        else if(size==='lg' && color==='dark')
-        {
-            setClassName(styles.buttonContainerDarklg)
-        }
-        else if(size==='lg' && color==='light')
-        {
-            setClassName(styles.buttonContainerLightlg)
-        }
-        else if(size==='xl' && color==='dark')
-        {
-            setClassName(styles.buttonContainerDarkxl)
-        }
-        else if(size==='xl' && color==='light')
-        {
-            setClassName(styles.buttonContainerDarkRedlg)
-        }
-        return ()=>{}
-    },[size,color])
+  
   return (
     <button 
         className={className}
